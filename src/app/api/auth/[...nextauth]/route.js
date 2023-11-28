f1a05f4cda58a464f71d062994de00b9333fd54d
rhjhaerol <rhjhaerol@gmail.com>
2023-11-28 11:43:45 +0700

    change route .ts jo .js

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,37 @@
+import { User } from "@/models/User";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
+        if (passwordOk) {
+          return user;
+        }
+
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
