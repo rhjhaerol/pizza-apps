6a66e2fb496eed32ece2ca96b8c5d274df86aa3d
rhjhaerol <rhjhaerol@gmail.com>
2023-12-07 11:40:51 +0700

    update user model

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,13 @@ const UserSchema = new Schema({
   name: {type: String},
   email: {type: String, required: true, unique: true},
   password: {type: String},
-  image: {type: String},
+  phone: {type: String},
+  address: {type: String},
+  postalcode: {type: String},
+  city: {type: String},
+  country: {type: String},
+  
+  
 }, {timestamps: true});
 
 export const User = models?.User || model('User', UserSchema);
