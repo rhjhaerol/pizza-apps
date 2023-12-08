e19f80a3e5d5d78c9c28721b89f3c18514fef2ef
rhjhaerol <rhjhaerol@gmail.com>
2023-12-08 10:52:11 +0700

    update layout page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,31 +2,33 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import AppProvider from "@/components/AppContext";
+import { Toaster } from "react-hot-toast";
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
+    subsets: ["latin"],
+    weight: ["400", "500", "700"],
 });
 
 export const metadata = {
-  title: "ST Pizza",
-  description: "Pizza Apps",
+    title: "ST Pizza",
+    description: "Pizza Apps",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={roboto.className}>
-        <main className="max-w-6xl mx-auto p-4">
-          <AppProvider>
-            <Header />
-            {children}
-            <footer className="border-t p-8 text-center text-gray-500 mt-16">
-              &copy; 2023 All right reserved
-            </footer>
-          </AppProvider>
-        </main>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={roboto.className}>
+                <main className="max-w-6xl mx-auto p-4">
+                    <AppProvider>
+                        <Toaster />
+                        <Header />
+                        {children}
+                        <footer className="border-t p-8 text-center text-gray-500 mt-16">
+                            &copy; 2023 All right reserved
+                        </footer>
+                    </AppProvider>
+                </main>
+            </body>
+        </html>
+    );
 }
