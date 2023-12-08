14a2d68d8a5087ad97f02a2d1b04d2d5e4b82b41
rhjhaerol <rhjhaerol@gmail.com>
2023-12-08 10:52:59 +0700

    update user models

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,18 @@
-import {model, models, Schema} from "mongoose";
+import { model, models, Schema } from "mongoose";
 
-const UserSchema = new Schema({
-  name: {type: String},
-  email: {type: String, required: true, unique: true},
-  password: {type: String},
-  phone: {type: String},
-  address: {type: String},
-  postalcode: {type: String},
-  city: {type: String},
-  country: {type: String},
-  
-  
-}, {timestamps: true});
+const UserSchema = new Schema(
+    {
+        name: { type: String },
+        email: { type: String, required: true, unique: true },
+        password: { type: String },
+        phone: { type: String },
+        address: { type: String },
+        postalcode: { type: String },
+        city: { type: String },
+        country: { type: String },
+        admin: { type: Boolean, default: false },
+    },
+    { timestamps: true }
+);
 
-export const User = models?.User || model('User', UserSchema);
+export const User = models?.User || model("User", UserSchema);
