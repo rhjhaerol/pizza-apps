5c316d62e8780363004582a2a345679897ac6e88
rhjhaerol <rhjhaerol@gmail.com>
2023-12-08 10:52:25 +0700

    add categories page

---
+++ src/app/categories/page.jsx
@@ -0,0 +1,13 @@
+import UserTab from "@/components/layout/UserTab";
+import React from "react";
+
+const page = () => {
+    return (
+        <section className="max-w-xl mx-auto mt-8">
+            <UserTab isAdmin={true} />
+            categories
+        </section>
+    );
+};
+
+export default page;
