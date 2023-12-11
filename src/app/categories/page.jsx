db78414c5b2b2e1f10ee7725dd72fb6308639c1e
rhjhaerol <rhjhaerol@gmail.com>
2023-12-11 10:47:03 +0700

    update categories page

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,11 +1,26 @@
+"use client";
+
 import UserTab from "@/components/layout/UserTab";
-import React from "react";
+import { UserProfile } from "@/components/UserProfile";
 
 const page = () => {
+    const { data: profileData, loading: profileLoading } = UserProfile();
+
+    if (profileLoading) {
+        return "Loading info user...";
+    }
+
+    if (!profileData) {
+        return "Not an admin";
+    }
+
     return (
-        <section className="max-w-xl mx-auto mt-8">
+        <section className="max-w-lg mx-auto mt-8">
             <UserTab isAdmin={true} />
-            categories
+            <form className="mt-8">
+                <label>New Category Name</label>
+                <input type="text" />
+            </form>
         </section>
     );
 };
