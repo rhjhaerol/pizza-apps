d4457717c84ae1518e4afc48ea7147b1dac28666
rhjhaerol <rhjhaerol@gmail.com>
2023-12-11 10:46:41 +0700

    update profile api

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -2,16 +2,21 @@ import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { User } from "@/models/User";
+import { UserInfo } from "@/models/UserInfo";
 
 export async function PUT(req) {
     mongoose.connect(process.env.MONGO_URL);
 
     const data = await req.json();
+    const { name, ...otherUserInfo } = data;
+
     const session = await getServerSession(authOptions);
 
     const email = session.user.email;
 
-    await User.updateOne({ email }, data);
+    await User.updateOne({ email }, name);
+
+    await UserInfo.findOneAndUpdate({ email }, otherUserInfo, { upsert: true });
 
     return Response.json(true);
 }
@@ -21,7 +26,14 @@ export async function GET(req) {
 
     const session = await getServerSession(authOptions);
 
-    const email = session.user.email;
+    const email = session?.user?.email;
+    if (!email) {
+        return Response.json({});
+    }
 
-    return Response.json(await User.findOne({ email }));
+    const user = await User.findOne({ email }).lean();
+
+    const userInfo = await UserInfo.findOne({ email }).lean();
+
+    return Response.json({ ...user, ...userInfo });
 }
